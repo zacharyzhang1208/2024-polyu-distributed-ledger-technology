46a205be4b34f4121318e0d10e6e8dc9b40ffc7a
zacharyzhang1208 <zacharyzhang1208@hotmail.com>
2024-11-22 23:18:08 +0800

    structural modification

--- front-end/webpack.config.js
+++ front-end/webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.jsx',
+  entry: './src/Index.jsx',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
