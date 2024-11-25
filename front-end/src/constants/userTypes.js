3860aee4f27306ec3a2ea567fe115aef0dd8c221
zacharyzhang1208 <zacharyzhang1208@hotmail.com>
2024-11-25 09:17:40 +0800

    profile route

--- front-end/src/constants/userTypes.js
+++ front-end/src/constants/userTypes.js
@@ -6,15 +6,17 @@ export const USER_TYPES = {
 // 定义每种用户类型的路由配置
 export const USER_ROUTES = {
     [USER_TYPES.STUDENT]: [
+        { path: '/profile', icon: 'fas fa-user', label: 'Profile' },
         { path: '/student_courses', icon: 'fas fa-book', label: 'My Courses' },
     ],
     [USER_TYPES.TEACHER]: [
+        { path: '/profile', icon: 'fas fa-user', label: 'Profile' },
         { path: '/teacher_courses', icon: 'fas fa-chalkboard-teacher', label: 'Teaching Courses' },
     ]
 };
 
 // 定义每种用户类型的默认首页路由
 export const DEFAULT_ROUTES = {
-    [USER_TYPES.STUDENT]: '/student_courses',
-    [USER_TYPES.TEACHER]: '/teacher_courses'
+    [USER_TYPES.STUDENT]: '/profile',
+    [USER_TYPES.TEACHER]: '/profile'
 };
